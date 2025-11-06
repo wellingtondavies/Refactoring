cf615780eed13b81d6cafc440f374b81ef4a83ed
wellingtondavies <wdsantos@minha.fag.edu.br>
2025-11-06 09:17:55 -0300

    Commit 04
    "Commit 4 - Move Method (amountFor → getCharge in Rental)"

--- Customer.java
+++ Customer.java
@@ -26,7 +26,8 @@ public class Customer {
          double thisAmount = 0;
          Rental aRental = (Rental) rentals.nextElement();
 
-         thisAmount = amountFor(aRental);
+         // chamada ao método movido
+         thisAmount = aRental.getCharge();
 
          // add frequent renter points
          frequentRenterPoints ++;
@@ -43,27 +44,6 @@ public class Customer {
       result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
       result += "You earned " + String.valueOf(frequentRenterPoints) +
               " frequent renter points";
-      return result;
-   }
-
-   private double amountFor(Rental aRental) {
-      double result = 0;
-
-      switch (aRental.getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            result += 2;
-            if (aRental.getDaysRented() > 2)
-               result += (aRental.getDaysRented() - 2) * 1.5;
-            break;
-         case Movie.NEW_RELEASE:
-            result += aRental.getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            result += 1.5;
-            if (aRental.getDaysRented() > 3)
-               result += (aRental.getDaysRented() - 3) * 1.5;
-            break;
-      }
       return result;
    }
 }
